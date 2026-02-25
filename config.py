33d42a3a25dc60485e02f588c82c1c751a9d70ad
neewait <121168120+neewait@users.noreply.github.com>
2026-02-25 17:58:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,16 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-in-prod!'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/app.db'
+    
+    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/securetest.db'
+    
+    
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {'check_same_thread': False}
+    }
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
-    
-    # Настройки для тестирования уязвимостей (не включать в prod!)
     DEBUG = True
     SHOW_SQL_QUERIES = True
